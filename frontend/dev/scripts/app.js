05b87ef023b5290068d6966605d989462e13089e
juan Izquierdo <practicante@code-labs.com>
2019-11-07 15:34:37 -0500

    "nada"

--- frontend/dev/scripts/app.js
+++ frontend/dev/scripts/app.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
   setTimeout(function(){
       $('section#login').fadeIn(2000);
   }, 500);
-<<<<<<< HEAD
-=======
 
 var wow = new WOW().init();
 
@@ -14,17 +12,5 @@ $('input[type="checkbox"]').click(function () {
   $(this).prop('checked') ? $(this).parent().text('a') : $(this).parent().text('b');
   console.log($(this));
 });
->>>>>>> e8fa123098a3d75c1f9c22a9d98890365e2e7001
 
-var wow = new WOW().init();
-
-<<<<<<< HEAD
-$('input[type="checkbox"]').click(function () {
-
-
-  $(this).prop('checked') ? $(this).parent().text('a') : $(this).parent().text('b');
-  console.log($(this));
-=======
-//https://icomoon.io/app/#/select
->>>>>>> e8fa123098a3d75c1f9c22a9d98890365e2e7001
 });
