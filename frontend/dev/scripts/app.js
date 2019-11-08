fbe760e837c89b718a8882146ab667930131f2b3
Jacobo <chandas.2001@gmail.com>
2019-11-08 16:16:03 -0500

    login 100$ maqueta

--- frontend/dev/scripts/app.js
+++ frontend/dev/scripts/app.js
@@ -1,178 +1,139 @@
 $('form').submit(function(event) {
-      event.preventDefault();
-      var camposObliga = $(this).find('input,select').length;
-      var botonSubmit = $(this).find('button[type="submit"]');
-      console.log(camposObliga);
-      // Desabilitar el boton submit
-      if (camposObliga != null) {
+  event.preventDefault();
 
-        botonSubmit.attr('disabled', true);
-        setTimeout(function() {
-          botonSubmit.attr('disabled', false);
-        }, 2000);
-      }
+  var camposObliga = $(this).find('input,select').length;
+  var botonSubmit = $(this).find('button[type="submit"]');
 
-
-
-      var cumple = 0;
-      //Arreglo donde almacenará cada campo
-      //expresiones Regulares
-      const regExpEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-      const regExpNumber = /^\d+$/;
-
-      ////Validar los campos
-      $(this).find('input,select').each(function(i, e) {
-
-        var valorInput = $(e).val();
-        var valorCheckbox = $(e).prop('checked');
-        var valorSelect = $(e).val();
-        var labelCheckBox = $(e).parent().find('label');
+  // Desabilitar el boton submit
+  if (camposObliga != null) {
+    botonSubmit.attr('disabled', true);
+    setTimeout(function() {
+      botonSubmit.attr('disabled', false);
+    }, 3000);
+  }
 
 
-        if ($(e).length !== 0) {
-          switch ($(e).attr('type')) {
-            case "text":
-              if (valorInput !== "") {
-                pintarStilos($(e), 'valido', true);
-              } else {
-                pintarStilos($(e), 'error', true);
-              }
-              break;
+  var total = 0;
+  //Arreglo donde almacenará cada campo
+  //expresiones Regulares
+  const regExpEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+  const regExpNumber = /^\d+$/;
 
-            case "number":
-              if ((regExpNumber.test($(e).prop('value'))) & (validarLength(valorInput.length, 5, 10))) {
-                pintarStilos($(e), 'valido', true);
-              } else {
-                pintarStilos($(e), 'error', true);
-              }
-              break;
+  ////Validar los campos
+  $(this).find('input,select').each(function(i, e) {
 
-            case "emailCustom":
-              if (regExpEmail.test($(e).prop('value'))) {
-                pintarStilos($(e), 'valido', true);
-              } else {
-                pintarStilos($(e), 'error', true);
-              }
-              break;
+    var valorInput = $(e).val();
+    var valorCheckbox = $(e).prop('checked');
+    var valorSelect = $(e).val();
+    var customDivCheck = $(e).parent().find('div.custom-checkbox');
 
-            case "checkbox":
-              if (valorCheckbox !== false) {
-                pintarStilos(labelCheckBox, 'valido', false);
-              } else {
-                pintarStilos(labelCheckBox, 'error', false);
-              }
-              break;
 
-            case "select":
-              if (valorSelect != 1) {
-                pintarStilos($(e), 'valido', true);
+    if ($(e).length !== 0) {
+      switch ($(e).attr('type')) {
 
-              } else {
-                pintarStilos($(e), 'error', true);
+        case "text":
+          valorInput !== "" ? pintarStilos($(e), 'valido') : pintarStilos($(e), 'error');
+          break;
 
-              }
-              break;
+        case "password":
+          valorInput !== "" ? pintarStilos($(e), 'valido') : pintarStilos($(e), 'error');
+          break;
 
-            
-
+        case "number":
+          if ((regExpNumber.test($(e).prop('value'))) & (validarLength(valorInput.length, 5, 10))) {
+            pintarStilos($(e), 'valido');
+          } else {
+            pintarStilos($(e), 'error');
           }
-        }
+          break;
 
-      });
-    });
+        case "emailCustom":
+          regExpEmail.test($(e).prop('value')) ? pintarStilos($(e), 'valido') : pintarStilos($(e), 'error');
+          break;
 
-    //////////// Pinta estilos de error y valido
-    function pintarStilos(elem, tipo) {
-       var contenedor = elem.parent();
-       //Iconos
+        case "checkbox":
+          valorCheckbox !== false ? pintarStilos(customDivCheck, 'valido') : pintarStilos(customDivCheck, 'error');
+          break;
 
-
-       if (tipo === 'error') {
-          elem.addClass('error');
-          elem.removeClass('correcto');
-          cumple = 0;
-
-       } else if (tipo === 'valido') {
-
-          elem.addClass('correcto');
-          elem.removeClass('error');
-          cumple++;
-
-       }
-
+        case "select":
+          valorSelect != 1 ? pintarStilos($(e), 'valido') : pintarStilos($(e), 'error');
+          break;
+      }
+      total = total + cumple;
     }
-    // function showAlert(cumple,timeIn,timeOut){
-    //
-    //    const iconAlertCheck = '<p class="icon-AlertAlertWrong"><span>Ha habido un error, recarga la página y vuelve a intentarlo</span></p>';
-    //    const iconAlertWrong = '<div class="icon-AlertAlertCheck"><div class="path1"></div><div class="path2"><span>Tu mensaje ha sido enviado con exito</span></div></div>';
-    //    const divAlert = 'div.alertForm-container';
-    //
-    //    $(divAlert).find('div.alertForm-content').empty();
-    //
-    //    if(cumple){
-    //       $(divAlert).find('div.alertForm-content').removeClass('alertError');
-    //       $(divAlert).find('div.alertForm-content').addClass('alertCheck');
-    //       $(divAlert).find('div.alertForm-content').append(iconAlertWrong);
-    //    }else{
-    //       $(divAlert).find('div.alertForm-content').removeClass('alertCheck');
-    //       $(divAlert).find('div.alertForm-content').addClass('alertError');
-    //       $(divAlert).find('div.alertForm-content').append(iconAlertCheck);
-    //    }
-    //    $(divAlert).fadeIn(timeIn,"linear",function(){
-    //       setTimeout(function(){
-    //          $(divAlert).fadeOut().animate({'bottom': '0%'}, {duration: 'slow', queue: false});
-    //       }, timeOut);
-    //    }).animate({'bottom': '10%'}, {duration: 'slow', queue: false});
-    //
-    // }
-    //
 
-    // //////////// Valida longitud
-    // function validarLength(valor, minLength, maxLength) {
-    //    if ((valor >= minLength) & (valor <= maxLength)) {
-    //       return true;
-    //    } else {
-    //       return false;
-    //    }
-    // }
-    // //////////// Valida Formulario
-    // function validarForm(camposObliga, form) {
-    //
-    //
-    //
-    //
-    //    });
-    //
-    //
-    //
-    //    if (cumple === camposObliga) {
-    //       alert('Bien pelado :D')
-    //       showAlert(false,500,10000)
-    //       return false;
-    //
-    //    } else {
-    //       return false;
-    //    }
-    //
-    //
-    //
-    // }
+  });
+  if(total === camposObliga){
+    showAlert(true,500,3000);
+
+  }else{
+    showAlert(false,500,3000);
+
+  }
+
+});
+
+//////////// Pinta estilos de error y valido
+function pintarStilos(elem, tipo) {
+
+  if (tipo === 'error') {
+    elem.addClass('error');
+    elem.removeClass('correcto');
+    cumple = 0;
+
+  } else if (tipo === 'valido') {
+    elem.addClass('correcto');
+    elem.removeClass('error');
+    cumple = 1;
+  }
+
+}
+function showAlert(cumple,timeIn,timeOut){
+
+   const divAlert = $('div.alert-container');
+   const contentAlert = 'div.alert-content';
+
+   divAlert.find('div.alert-content').empty();
 
 
-    $(document).ready(function() {
-      $('section#login').hide();
-      setTimeout(function() {
-        $('section#login').fadeIn(2000);
-      }, 500);
+   if(cumple){
+      divAlert.removeClass('alertError');
+      divAlert.addClass('alertCheck').find(contentAlert).text('Nice !');
+   }else{
+      divAlert.removeClass('alertCheck');
+      divAlert.addClass('alertError').find(contentAlert).text('Hmm, something went wrong');
+   }
 
-      var wow = new WOW().init();
+   divAlert.fadeIn(timeIn,"linear",function(){
+      setTimeout(function(){
+         divAlert.fadeOut().animate({'bottom': '0%'}, {duration: 'slow', queue: false});
+      }, timeOut);
+   }).animate({'bottom': '15%'}, {duration: 'slow', queue: false});
 
-      $('input[type="checkbox"]').click(function() {
+}
 
 
-        $(this).prop('checked') ? $(this).parent().toggleClass('checked') : $(this).parent().toggleClass('checked');
-        console.log($(this).prop('checked'));
-        console.log($(this).parent().before());
-      });
+//////////// Valida longitud
+function validarLength(valor, minLength, maxLength) {
+   if ((valor >= minLength) & (valor <= maxLength)) {
+      return true;
+   } else {
+      return false;
+   }
+}
 
-    });
+
+$(document).ready(function() {
+  $('section#login').hide();
+  setTimeout(function() {
+    $('section#login').fadeIn(2000);
+  }, 500);
+
+  var wow = new WOW().init();
+
+  $('input[type="checkbox"]').click(function() {
+    let customCheck = $(this).parent().find('div.custom-checkbox');
+    $(this).prop('checked') ? customCheck.text('✔') : customCheck.text('');
+  });
+
+});
