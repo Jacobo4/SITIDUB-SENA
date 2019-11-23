9a1bfbde47d29181c443dcf9fe5c177cb622ecc5
Jacobo <chandas.2001@gmail.com>
2019-11-23 11:48:29 -0500

    campos del mer corregidos

--- docs/scriptBase.sql
+++ docs/scriptBase.sql
@@ -1,12 +1,10 @@
--- -----------------------------------------------------
--- Schema mydb
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema proyecto
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `proyecto` ;
-USE `proyecto` ;
+drop database if exists proyecto;
+CREATE SCHEMA IF NOT EXISTS proyecto ;
+USE proyecto ;
 
 -- -----------------------------------------------------
 -- Table `proyecto`.`tipos_documentos`
@@ -20,9 +18,9 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`tipos_documentos` (
 -- Table `proyecto`.`observaciones`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `proyecto`.`observaciones` (
-  `tipo_observacion` VARCHAR(15) NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `des_observacion` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`tipo_observacion`));
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -50,29 +48,25 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`personas` (
   `fecha_nacimiento` DATE NOT NULL,
   `direccion` VARCHAR(45) NOT NULL,
   `email` VARCHAR(50) NOT NULL,
-  `observacion_persona` VARCHAR(15) NULL DEFAULT NULL,
-  `tel1` TINYINT(12) NOT NULL,
-  `tel2` TINYINT(12) NULL,
-  `tel3` TINYINT(12) NULL,
-  `ocupacion` VARCHAR(35) NOT NULL,
-  `profesion` VARCHAR(35) NOT NULL,
+  `id_observacion` INT NULL DEFAULT NULL,
+  `tel1` VARCHAR(12) NOT NULL,
+  `tel2` VARCHAR(12) NULL DEFAULT NULL,
+  `tel3` VARCHAR(12) NULL DEFAULT NULL,
+  `ocupacion` VARCHAR(35) NULL DEFAULT NULL,
+  `profesion` VARCHAR(35) NULL DEFAULT NULL,
   `rh` VARCHAR(5) NOT NULL,
   `estrato` INT NOT NULL,
   `eps_des_eps` VARCHAR(30) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_tbl_persona_tbl_tipoDocumento1` (`tdoc_persona` ASC) VISIBLE,
-  INDEX `fk_tbl_persona_tbl_observaciones1` (`observacion_persona` ASC) VISIBLE,
-  INDEX `fk_personas_eps1_idx` (`eps_des_eps` ASC) VISIBLE,
-  UNIQUE INDEX `ndoc_UNIQUE` (`ndoc` ASC) VISIBLE,
-  UNIQUE INDEX `tdoc_persona_UNIQUE` (`tdoc_persona` ASC) VISIBLE,
+  UNIQUE INDEX `ndoc_UNIQUE` (`ndoc` ASC),
   CONSTRAINT `fk_tbl_persona_tbl_tipoDocumento1`
     FOREIGN KEY (`tdoc_persona`)
     REFERENCES `proyecto`.`tipos_documentos` (`des_tipoDocumento`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_tbl_persona_tbl_observaciones1`
-    FOREIGN KEY (`observacion_persona`)
-    REFERENCES `proyecto`.`observaciones` (`tipo_observacion`)
+    FOREIGN KEY (`id_observacion`)
+    REFERENCES `proyecto`.`observaciones` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `fk_personas_eps1`
@@ -93,7 +87,6 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`matriculas` (
   `grado` VARCHAR(15) NULL DEFAULT NULL,
   `personas_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_matriculas_personas1_idx` (`personas_id` ASC) VISIBLE,
   CONSTRAINT `fk_matriculas_personas1`
     FOREIGN KEY (`personas_id`)
     REFERENCES `proyecto`.`personas` (`id`)
@@ -111,7 +104,6 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`cuotas` (
   `saldo` FLOAT NOT NULL,
   `id_matricula` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_cuotas_matriculas1_idx` (`id_matricula` ASC) VISIBLE,
   CONSTRAINT `fk_cuotas_matriculas1`
     FOREIGN KEY (`id_matricula`)
     REFERENCES `proyecto`.`matriculas` (`id`)
@@ -128,10 +120,11 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`pagos` (
   `fecha_pago` DATE NOT NULL,
   `periodo_inicial` DATE NOT NULL,
   `periodo_final` DATE NOT NULL,
+  `valor_cancelado` FLOAT NOT NULL,
   `rector` VARCHAR(30) NOT NULL,
   `id_cuota` INT NOT NULL,
-  PRIMARY KEY (`id`, `consecutivo`),
-  INDEX `fk_pagos_cuotas1_idx` (`id_cuota` ASC) VISIBLE,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `consecutivo_UNIQUE` (`consecutivo` ASC),
   CONSTRAINT `fk_pagos_cuotas1`
     FOREIGN KEY (`id_cuota`)
     REFERENCES `proyecto`.`cuotas` (`id`)
@@ -147,7 +140,6 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`parentescos` (
   `id_estudiante` INT NOT NULL,
   `id_acudiente` INT NOT NULL,
   PRIMARY KEY (`id_estudiante`, `id_acudiente`),
-  INDEX `fk_parentescos_personas2_idx` (`id_acudiente` ASC) VISIBLE,
   CONSTRAINT `fk_parentescos_personas1`
     FOREIGN KEY (`id_estudiante`)
     REFERENCES `proyecto`.`personas` (`id`)
@@ -158,3 +150,59 @@ CREATE TABLE IF NOT EXISTS `proyecto`.`parentescos` (
     REFERENCES `proyecto`.`personas` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
+INSERT INTO tipos_documentos (des_tipoDocumento) VALUES
+('CC'),
+('CE'),
+('TI');
+
+INSERT INTO eps (des_eps) VALUES
+('Compensar'),
+('Sanitas'),
+('ASAAP');
+
+INSERT INTO personas (id, ndoc, tdoc_persona, tipo_persona, nombre1, nombre2, apellido1, apellido2, lugar_expedicion, lugar_nacimiento, fecha_nacimiento, direccion, email, id_observacion, tel1, tel2, tel3, ocupacion, profesion, rh, estrato, eps_des_eps) VALUES
+(null, '1001097692', 'TI', 'estudiante', 'Juan', 'Jacobo', 'Izquierdo', 'Becerra', 'Bogotá', 'Bogotá', '2001-03-28', 'Calle A', 'jacobo@gmail.com', null, 3058194685, 313202902, null, null, null, 'O+', '3', 'Compensar'),
+(null, '1001097693', 'TI', 'estudiante', 'Camila', null, 'Izquierdo', 'Camacho', 'Bogotá', 'Bogotá', '2001-07-18', 'Calle A', 'camila@gmail.com', null, 369874521, null, null, null, null, 'A+', '3', 'Sanitas'),
+(null, '3546976', 'CC', 'responsable', 'Adelaida', null, 'Becerra', 'Cano', 'Bogotá', 'Bogotá', '1956-03-28', 'Calle A', 'adeliada@gmail.com', null, 3154207893, null, null, null, null, 'O-', '3', 'Compensar');
+
+
+INSERT INTO parentescos (parentesco, id_estudiante, id_acudiente) VALUES
+('mama', '1', '3'),
+('tia', '2', '3');
+
+
+INSERT INTO matriculas (id, fecha_inicial, fecha_final, estado, grado, personas_id) VALUES
+(null, '2018-02-24', null, '1', 'octavo', '1'),
+(null, '2018-02-24', null, '1', 'septimo', '2');
+
+
+INSERT INTO cuotas (id, mes, valor, saldo, id_matricula) VALUES
+(null, 'febrero', '500000', '500000', '1'),
+(null, 'marzo', '300000', '300000', '1'),
+(null, 'abril', '300000', '300000', '1'),
+(null, 'mayo', '300000', '300000', '1'),
+(null, 'junio', '300000', '300000', '1'),
+(null, 'julio', '300000', '300000', '1'),
+(null, 'agosto', '300000', '300000', '1'),
+(null, 'septiembre', '300000', '300000', '1'),
+(null, 'octubre', '300000', '300000', '1'),
+(null, 'noviembre', '300000', '300000', '1'),
+
+(null, 'febrero', '500000', '500000', '2'),
+(null, 'marzo', '300000', '300000', '2'),
+(null, 'abril', '300000', '300000', '2'),
+(null, 'mayo', '300000', '300000', '2'),
+(null, 'junio', '300000', '300000', '2'),
+(null, 'julio', '300000', '300000', '2'),
+(null, 'agosto', '300000', '300000', '2'),
+(null, 'septiembre', '300000', '300000', '2'),
+(null, 'octubre', '300000', '300000', '2'),
+(null, 'noviembre', '300000', '300000', '2');
+
+
+INSERT INTO pagos (id, consecutivo, fecha_pago, periodo_inicial, periodo_final, valor_cancelado, rector, id_cuota) VALUES 
+(null, '1234', '2018-03-02', '2018-02-02', '2018-03-02', '300000', 'Pereson', '1'),
+(null, '12345', '2018-03-05', '2018-02-02', '2018-03-02', '200000', 'Pereson', '1'),
+(null, '45645', '2018-04-02', '2018-02-02', '2018-03-02', '500000', 'Pereson', '11'),
+(null, '12312', '2018-04-02', '2018-03-02', '2018-04-02', '300000', 'Pereson', '12');
